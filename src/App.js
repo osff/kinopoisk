1f29a601c914f899b5fdffb299a49c9ff3dff5ee
Oleg Samokhin <oleg.samokhin.firefly@gmail.com>
2017-06-30 09:29:47 +0500

    styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import { StyleSheet, css } from 'aphrodite';
 import logo from './logo.svg';
 import './App.css';
 
-class MovieListItem extends Component {
+class MovieListItemRatingDropdown extends Component {
+  handleChange() {
+
+  }
+
   render() {
     return (
-      <div className="movie-wrapper">
-        {this.props.name} {this.props.actors} {this.props.rating}
+      <select value={this.props.value} onChange={this.handleChange}>
+       <option value="1">1</option>
+       <option value="2">2</option>
+       <option value="3">3</option>
+       <option value="4">4</option>
+       <option value="5">5</option>
+     </select>
+    );
+  }
+}
+
+class MovieListItem extends Component {
+  render() {
+    const actors = this.props.actors.join(", ");
+    return (
+      <div className={css(styles.movieWrapper)}>
+        <div>
+          <img className={css(styles.movieImage)} src={this.props.image} />
+        </div>
+        <div className={css(styles.movieDescription)}>
+          <h1>{this.props.name} ({this.props.year})</h1>
+          Actors: {actors} <br />
+          Rating: <MovieListItemRatingDropdown value={this.props.rating} />
+        </div>
+
       </div>
     );
   }
@@ -15,9 +43,14 @@ class MovieListItem extends Component {
 class MovieList extends Component {
   render() {
     const moviesListItems = this.props.movies.map((movie) =>
-      <MovieListItem key={movie.id}
-        name={movie.name} actors={movie.actors}
-        rating={movie.rating} />
+      <MovieListItem
+        key={movie.id}
+        image={movie.image}
+        name={movie.name}
+        year={movie.year}
+        actors={movie.actors}
+        rating={movie.rating}
+      />
     );
     return (
       <div className="movie-list">
@@ -30,11 +63,11 @@ class MovieList extends Component {
 class App extends Component {
   render() {
     const movies = [
-      { id: 1, name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 5 },
-      { id: 2, name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 4 },
-      { id: 3, name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 3 },
-      { id: 4, name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 2 },
-      { id: 5, name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 1 },
+      { id: 1, image: "https://st.kp.yandex.net/images/film_iphone/iphone360_342.jpg", year: "1994", name: "Pulp fiction", actors: ['John Travolta', 'Samuel L. Jackson',], rating: 5 },
+      { id: 2, image: "https://st.kp.yandex.net/images/film_iphone/iphone360_326.jpg", year: "1994", name: "The Shawshank Redemption", actors: ['Tim Robbins', 'Morgan Freeman',], rating: 4 },
+      { id: 3, image: "https://st.kp.yandex.net/images/film_iphone/iphone360_435.jpg", year: "1999", name: "The Green Mile", actors: ['Tom Henks', 'David Mors',], rating: 3 },
+      { id: 4, image: "https://st.kp.yandex.net/images/film_iphone/iphone360_448.jpg", year: "1994", name: "Forrest Gump", actors: ['Tom Henks', 'Robin Rite',], rating: 2 },
+      { id: 5, image: "https://st.kp.yandex.net/images/film_iphone/iphone360_329.jpg", year: "1993", name: "Schindler's List", actors: ['Liam Nisson', 'Ben Ringslie',], rating: 1 },
     ]
     return (
       <div className="App">
@@ -49,3 +82,21 @@ class App extends Component {
 }
 
 export default App;
+
+const styles = StyleSheet.create({
+    movieWrapper: {
+        display: 'flex',
+        justifyContent: 'center',
+        width: "80vw",
+        margin: "1em auto"
+    },
+
+    movieDescription: {
+        padding: '1em 0 0 2em',
+        width: "50%"
+    },
+
+    movieImage: {
+      width: "70%"
+    }
+});
